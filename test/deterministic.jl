11c3fbf25ed6be1588248d8b9297e2cfb3827ac1
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-01-02 22:06:25 +0100

    Improve DeterministicNode tests

--- test/deterministic.jl
+++ test/deterministic.jl
@@ -2,34 +2,43 @@
 # Copyright (c) 2022, Institute of Automatic Control - RWTH Aachen University
 # All rights reserved. 
 
-rng = Random.default_rng()
+@testset "DeterministicNode, RNG: $rng" for rng in rngs
+    a_params = array_for_rng(rng, Float32, 3)
+    a_params .= 1
+    a = BroadcastedNode(:a, rng, KernelExponential, a_params)
+    b_params = array_for_rng(rng, Float32, 2)
+    b_params .= 1
+    b = BroadcastedNode(:b, rng, KernelExponential, b_params)
 
-a = BroadcastedNode(:a, rng, KernelExponential, [1.0f0, 2, 3])
-b = BroadcastedNode(:b, rng, KernelExponential, [1.0f0, 2])
+    fn(a, ::Any) = a
+    c = DeterministicNode(:c, fn, (; a=a, b=b))
 
-fn(a, ::Any) = a
-c = DeterministicNode(:c, fn, (; a=a, b=b))
+    nt = rand(c)
+    @test nt.c isa AbstractArray{Float32,1}
+    @test size(nt.c) == (3,)
+    ℓ = logdensityof(c, nt)
+    @test ℓ isa Float32
+    @test ℓ == logdensityof(a, nt) + logdensityof(b, nt)
 
-nt = rand(c)
-@test nt.c isa Array{Float32,1}
-@test size(nt.c) == (3,)
-ℓ = logdensityof(c, nt)
-@test ℓ isa Float32
-@test ℓ == logdensityof(a, nt) + logdensityof(b, nt)
+    nt = rand(c, 2)
+    @test nt.c isa AbstractArray{Float32,2}
+    @test size(nt.c) == (3, 2)
+    ℓ = logdensityof(c, nt)
+    @test ℓ isa AbstractArray{Float32,1}
+    @test ℓ == logdensityof(a, nt) + logdensityof(b, nt)
 
-nt = rand(c, 2)
-@test nt.c isa Array{Float32,2}
-@test size(nt.c) == (3, 2)
-ℓ = logdensityof(c, nt)
-@test ℓ isa Array{Float32,1}
-@test ℓ == logdensityof(a, nt) + logdensityof(b, nt)
+    # Test that evaluate only calls the DeterministicNode
+    nt = evaluate(c, (; a=1, b=2, c=3))
+    @test nt == (; a=1, b=2, c=1)
 
-# Test that evaluate only calls the DeterministicNode
-nt = evaluate(c, (; a=1, b=2, c=3))
-@test nt == (; a=1, b=2, c=1)
+    # Type stable in SequentializedGraph?
+    seq_graph = sequentialize(c)
+    nt = @inferred evaluate(seq_graph, (; a=1, b=2, c=3))
+    @test nt == (; a=1, b=2, c=1)
 
-# Deterministic as leaf
-d = DeterministicNode(:d, () -> fill(42, 42), (;))
-s = @inferred rand(d)
-@test logdensityof(d, s) == 0
-@test s.d == fill(42, 42)
+    # Deterministic as leaf
+    d = DeterministicNode(:d, () -> fill(42, 42), (;))
+    s = @inferred rand(d)
+    @test logdensityof(d, s) == 0
+    @test s.d == fill(42, 42)
+end
