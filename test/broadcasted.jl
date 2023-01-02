616f44ef308df64518aedf3a133389484d01b315
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-01-02 21:59:23 +0100

    Improve tests of BroadcastedNode and SequentializedGraph

--- test/broadcasted.jl
+++ test/broadcasted.jl
@@ -2,72 +2,86 @@
 # Copyright (c) 2022, Institute of Automatic Control - RWTH Aachen University
 # All rights reserved. 
 
-rng = Random.default_rng()
+@testset "BroadcastedNode, RNG: $rng" for rng in rngs
+    a_params = array_for_rng(rng, Float32, 3)
+    a_params .= 1
+    a = BroadcastedNode(:a, rng, KernelUniform, 0, a_params)
 
-a = BroadcastedNode(:a, rng, KernelUniform, 0, fill(1.0f0, 3))
-nt = @inferred rand(a)
-@test nt.a isa Array{Float32,1}
-@test size(nt.a) == (3,)
-ℓ = @inferred logdensityof(a, nt)
-@test ℓ isa Float32
-@test ℓ == 0
-# Multiple times
-nt = @inferred rand(a, 2)
-@test nt.a isa Array{Float32,2}
-@test size(nt.a) == (3, 2)
-ℓ = @inferred logdensityof(a, nt)
-@test ℓ isa Array{Float32,1}
-@test size(ℓ) == (2,)
-@test ℓ == [0, 0]
+    # single rand
+    nt = @inferred rand(a)
+    @test nt.a isa AbstractArray{Float32,1}
+    @test size(nt.a) == (3,)
+    ℓ = @inferred logdensityof(a, nt)
+    @test ℓ isa Float32
+    @test ℓ == 0
 
-b = BroadcastedNode(:b, rng, KernelExponential, fill(1.0f0, 3, 4))
-c = BroadcastedNode(:c, rng, KernelNormal, (; a=a, b=b))
-nt = rand(c)
-@test nt.c isa Array{Float32,2}
-@test size(nt.c) == (3, 4)
-ℓ = logdensityof(c, nt)
-@test ℓ isa Float32
-# Multiple samples
-nt = rand(c, 2)
-@test nt.c isa Array{Float32,3}
-@test size(nt.c) == (3, 4, 2)
-ℓ = logdensityof(c, nt)
-@test ℓ isa Array{Float32,1}
-@test size(ℓ) == (2,)
+    # multiple rand
+    nt = @inferred rand(a, 2)
+    @test nt.a isa AbstractArray{Float32,2}
+    @test size(nt.a) == (3, 2)
+    ℓ = @inferred logdensityof(a, nt)
+    @test ℓ isa AbstractArray{Float32,1}
+    @test size(ℓ) == (2,)
+    res = array_for_rng(rng, Float32, 2)
+    res .= 0
+    @test ℓ == res
 
-# Test logdensityof calculation
-d = BroadcastedNode(:d, rng, KernelNormal, (; c=c, b=b))
-nt = rand(d)
-ℓ = logdensityof(d, nt)
-@test ℓ isa Float32
-@test ℓ ≈ sum(logdensityof.(KernelUniform(), nt.a) .+ logdensityof.(KernelExponential(), nt.b) .+ logdensityof.(KernelNormal.(nt.a, nt.b), nt.c) .+ logdensityof.(KernelNormal.(nt.c, nt.b), nt.d))
+    # Graph
+    b_params = array_for_rng(rng, Float32, 3, 4)
+    b_params .= 1
+    b = BroadcastedNode(:b, rng, KernelExponential, b_params)
+    c = BroadcastedNode(:c, rng, KernelNormal, (; a=a, b=b))
 
-nt = rand(d, 2)
-ℓ = logdensityof(d, nt)
-@test ℓ isa AbstractArray{Float32,1}
-a_val = reshape(nt.a, 3, 1, 2)
-b_val, c_val, d_val = nt.b, nt.c, nt.d
-broadcasted_sum = logdensityof.(KernelUniform(), a_val) .+ logdensityof.(KernelExponential(), b_val) .+ logdensityof.(KernelNormal.(a_val, b_val), c_val) .+ logdensityof.(KernelNormal.(c_val, b_val), d_val)
-@test ℓ ≈ dropdims(sum(broadcasted_sum; dims=(1, 2)); dims=(1, 2))
+    # single rand
+    nt = rand(c)
+    @test nt.c isa AbstractArray{Float32,2}
+    @test size(nt.c) == (3, 4)
+    ℓ = logdensityof(c, nt)
+    @test ℓ isa Float32
 
-# Test bijectors
-bij = bijector(d)
-@test bij isa NamedTuple{(:a, :b, :c, :d)}
-a_bij = bijector(BroadcastedDistribution(KernelUniform, 0, fill(1.0f0, 3)))
-@test bij.a.dims == a_bij.dims
-@test materialize(bij.a.bijectors) == materialize(a_bij.bijectors)
-b_bij = bijector(BroadcastedDistribution(KernelExponential, fill(1.0f0, 3, 4)))
-@test bij.b.dims == b_bij.dims
-@test materialize(bij.b.bijectors) == materialize(b_bij.bijectors)
-c_bij = bijector(BroadcastedDistribution(KernelNormal, 0, fill(1.0f0, 3, 4)))
-@test bij.c.dims == c_bij.dims
-@test materialize(bij.c.bijectors) == materialize(c_bij.bijectors)
-d_bij = bijector(BroadcastedDistribution(KernelNormal, 0, fill(1.0f0, 3, 4)))
-@test bij.d.dims == d_bij.dims
-@test materialize(bij.d.bijectors) == materialize(d_bij.bijectors)
+    # multiple rand
+    nt = rand(c, 2)
+    @test nt.c isa AbstractArray{Float32,3}
+    @test size(nt.c) == (3, 4, 2)
+    ℓ = logdensityof(c, nt)
+    @test ℓ isa AbstractArray{Float32,1}
+    @test size(ℓ) == (2,)
 
-# Do the bijectors work for multiple samples?
-@test a_bij(nt.a) == bijector(KernelUniform(0, 1.0f0)).(nt.a)
-@test b_bij(nt.b) == bijector(KernelExponential(1.0f0)).(nt.b)
-@test c_bij(nt.c) == bijector(KernelNormal(0, 1.0f0)).(nt.c)
-@test d_bij(nt.d) == bijector(KernelNormal(0, 1.0f0)).(nt.d)
+    # logdensityof for single sample
+    d = BroadcastedNode(:d, rng, KernelNormal, (; c=c, b=b))
+    nt = rand(d)
+    ℓ = logdensityof(d, nt)
+    @test ℓ isa Float32
+    @test ℓ ≈ sum(logdensityof.(KernelUniform(), nt.a) .+ logdensityof.(KernelExponential(), nt.b) .+ logdensityof.(KernelNormal.(nt.a, nt.b), nt.c) .+ logdensityof.(KernelNormal.(nt.c, nt.b), nt.d))
+
+    # logdensityof for multiple samples
+    nt = rand(d, 2)
+    ℓ = logdensityof(d, nt)
+    @test ℓ isa AbstractArray{Float32,1}
+    a_val = reshape(nt.a, 3, 1, 2)
+    b_val, c_val, d_val = nt.b, nt.c, nt.d
+    broadcasted_sum = logdensityof.(KernelUniform(), a_val) .+ logdensityof.(KernelExponential(), b_val) .+ logdensityof.(KernelNormal.(a_val, b_val), c_val) .+ logdensityof.(KernelNormal.(c_val, b_val), d_val)
+    @test ℓ ≈ dropdims(sum(broadcasted_sum; dims=(1, 2)); dims=(1, 2))
+
+    # Bijectors
+    bij = bijector(d)
+    @test bij isa NamedTuple{(:a, :b, :c, :d)}
+    a_bij = bijector(BroadcastedDistribution(KernelUniform, 0, a_params))
+    @test bij.a.dims == a_bij.dims
+    @test materialize(bij.a.bijectors) == materialize(a_bij.bijectors)
+    b_bij = bijector(BroadcastedDistribution(KernelExponential, b_params))
+    @test bij.b.dims == b_bij.dims
+    @test materialize(bij.b.bijectors) == materialize(b_bij.bijectors)
+    c_bij = bijector(BroadcastedDistribution(KernelNormal, 0, b_params))
+    @test bij.c.dims == c_bij.dims
+    @test materialize(bij.c.bijectors) == materialize(c_bij.bijectors)
+    d_bij = bijector(BroadcastedDistribution(KernelNormal, 0, b_params))
+    @test bij.d.dims == d_bij.dims
+    @test materialize(bij.d.bijectors) == materialize(d_bij.bijectors)
+
+    # Do the bijectors work for multiple samples?
+    @test a_bij(nt.a) == bijector(KernelUniform(0, 1.0f0)).(nt.a)
+    @test b_bij(nt.b) == bijector(KernelExponential(1.0f0)).(nt.b)
+    @test c_bij(nt.c) == bijector(KernelNormal(0, 1.0f0)).(nt.c)
+    @test d_bij(nt.d) == bijector(KernelNormal(0, 1.0f0)).(nt.d)
+end
