fcd5deb53db1cf7aca170ef019d235cb9d3d2579
Tim Redick <tim.redick@rwth-aachen.de>
2024-04-23 20:14:32 +0200

    CUDA tests only if CUDA is functional

--- test/math.jl
+++ test/math.jl
@@ -1,6 +1,6 @@
 # @license BSD-3 https://opensource.org/licenses/BSD-3-Clause
 # Copyright (c) 2023, Institute of Automatic Control - RWTH Aachen University
-# All rights reserved. 
+# All rights reserved.
 
 @testset "add_logdensity scalar" begin
     y = @inferred add_logdensity(1, 2)
@@ -9,10 +9,12 @@
     @test y == [3]
     y = @inferred add_logdensity([1, 2], 1)
     @test y == [2, 3]
-    y = @inferred add_logdensity(1, CuArray([1, 2]))
-    @test y == CuArray([2, 3])
-    y = @inferred add_logdensity(CuArray([1, 2]), 1)
-    @test y == CuArray([2, 3])
+    if CUDA.functional()
+        y = @inferred add_logdensity(1, CuArray([1, 2]))
+        @test y == CuArray([2, 3])
+        y = @inferred add_logdensity(CuArray([1, 2]), 1)
+        @test y == CuArray([2, 3])
+    end
 end
 
 @testset "add_logdensity same array type" begin
@@ -20,15 +22,19 @@ end
     @test y == [2 4; 4 6; 6 8]
     y = @inferred add_logdensity([1 2], [1 2; 3 4; 5 6])
     @test y == [2 4; 4 6; 6 8]
-    y = @inferred add_logdensity(CuArray([1 2; 3 4; 5 6]), CuArray([1 2]))
-    @test y == CuArray([2 4; 4 6; 6 8])
-    y = @inferred add_logdensity(CuArray([1 2]), CuArray([1 2; 3 4; 5 6]))
-    @test y == CuArray([2 4; 4 6; 6 8])
+    if CUDA.functional()
+        y = @inferred add_logdensity(CuArray([1 2; 3 4; 5 6]), CuArray([1 2]))
+        @test y == CuArray([2 4; 4 6; 6 8])
+        y = @inferred add_logdensity(CuArray([1 2]), CuArray([1 2; 3 4; 5 6]))
+        @test y == CuArray([2 4; 4 6; 6 8])
+    end
 end
 
-@testset "add_logdensity different array type" begin
-    y = @inferred add_logdensity([1 2; 3 4; 5 6], CuArray([1 2]))
-    @test y == [2 4; 4 6; 6 8]
-    y = @inferred add_logdensity([1 2], CuArray([1 2; 3 4; 5 6]))
-    @test y == [2 4; 4 6; 6 8]
+if CUDA.functional()
+    @testset "add_logdensity different array type" begin
+        y = @inferred add_logdensity([1 2; 3 4; 5 6], CuArray([1 2]))
+        @test y == [2 4; 4 6; 6 8]
+        y = @inferred add_logdensity([1 2], CuArray([1 2; 3 4; 5 6]))
+        @test y == [2 4; 4 6; 6 8]
+    end
 end
