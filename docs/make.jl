e49f3507e63510fa642cdace5385eea90c0f6ea8
Tim Redick <tim.redick@rwth-aachen.de>
2025-04-29 08:43:42 +0200

    Update rwth-irt URLs to Tuebel

--- docs/make.jl
+++ docs/make.jl
@@ -6,4 +6,4 @@ using Documenter, BayesNet
 import Documenter.Remotes: GitLab
 
 makedocs(sitename="BayesNet")
-deploydocs(repo="github.com//BayesNet.jl.git")
+deploydocs(repo="github.com/Tuebel/BayesNet.jl.git")
